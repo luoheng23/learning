7c3af4b733f7fd53f8ff5adc571faac72747e702
luoheng <luoheng>
2018-08-15 16:59:08 +0800

    generator test2

--- generator_test.py
+++ generator_test.py
@@ -1,5 +1,16 @@
 
-def generator_test():
+# global variable
+num = 10
+
+def closure_test():
+    # 0...9 generator
+    x = (i for i in range(10))
+
+    def _closure_test():
+        return sum(x) * num
+    return _closure_test
+
+def generator_test1():
     # 0...9 generator
     x = (i for i in range(10))
     # 5..9 generator
@@ -11,5 +22,18 @@ def generator_test():
     l = list(x_filter)
     print("l, x_filter", l)
 
+def generator_test2():
+    x = (i for i in range(10))
+    for i in range(10):
+        x = (j + i for j in x)
+    L = list(x)
+    print("L, x", L)
+
+
+
 if __name__ == "__main__":
-    generator_test()
+    generator_test1()
+    generator_test2()
+    function = closure_test()
+    num = 20
+    print(function())
