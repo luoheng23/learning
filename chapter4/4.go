3b6f4401ae27be25f3d31e14dcc16eaad43f4b82
luoheng <1301089462@qq.com>
2018-11-11 15:20:40 +0800

    chapter4

---
+++ chapter4/4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func hello() {
+	fmt.Println("hello, that's good enough for everyone, but I want to have that in that case.")
+}
+
+func main() {
+	fmt.Println("hello, world!")
+	hello()
+}
