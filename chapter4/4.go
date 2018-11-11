df008b3f587e4bac20bd8b09fc29b203277a003d
luoheng <1301089462@qq.com>
2018-11-11 20:08:09 +0800

    chapter5-6

--- chapter4/4.go
+++ chapter4/4.go
@@ -6,6 +6,16 @@ func hello() {
 	fmt.Println("hello, that's good enough for everyone, but I want to have that in that case.")
 }
 
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	return strings[:i]
+}
 func main() {
 	fmt.Println("hello, world!")
 	hello()
