3d0a610474423b4438452b1cd5e6f9354c5ef030
luoheng <1301089462@qq.com>
2019-06-01 16:03:45 +0800

    add hello-world.sh

---
+++ hello-world.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# Program:
+#   This program shows "Hello world!" in your screen.
+# History:
+# 2015/07/16  VBird  First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin/:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+echo "Hello World!\a\n"
+exit 0
