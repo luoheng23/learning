9f812f4c8fea1885a1de4155a52b097cd2f7b922
luoheng <1301089462@qq.com>
2019-06-01 16:19:31 +0800

    modify hello-world.sh as the template file

--- hello-world.sh
+++ hello-world.sh
@@ -2,7 +2,7 @@
 # Program:
 #   This program shows "Hello world!" in your screen.
 # History:
-# 2015/07/16  VBird  First release
+# 2019/06/01  luoheng  First release
 PATH=/bin:/sbin:/usr/bin:/usr/sbin/:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 echo "Hello World!\a\n"
