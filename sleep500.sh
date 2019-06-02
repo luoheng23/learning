ef4177e4123620ceddaaf5a953b979c6ab49e6cf
luoheng <1301089462@qq.com>
2019-06-02 13:01:17 +0800

    sleep for 500 seconds This is a script to test nohup command

---
+++ sleep500.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# Program:
+#   sleep for 500 s
+# History:
+# 2019/06/01  luoheng  First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin/:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+sleep 500s
+echo "I have slept 500 seconds"
