5c3c2073f1e483bacd705562aa58a96ca88c22bc
luoheng <1301089462@qq.com>
2019-07-28 11:31:16 +0800

    variables ok

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+// var numOfStopLights = "Four"
+
+var numOfStopLights: Int = 4
+// const
+let numOfStopLigths: Int = 4
+
+var population: Int
+population = 5422
+
+let townName = "Knowhere"
+
+// interpolation
+let townDescription = "\(townName) has a population of \(population) and \(numOfStopLigths) stoplights."
