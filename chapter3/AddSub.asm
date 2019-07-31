2d307bb0f7763e0fa58a8825e4f47099ef61f501
luoheng <1301089462@qq.com>
2019-07-31 15:04:25 +0800

    add and sub

---
+++ chapter3/AddSub.asm
@@ -0,0 +1,13 @@
+; this program adds and substracts 32-bit integers.
+
+INCLUDE Irvine32.inc
+
+.code
+main PROC
+	mov eax, 10000h
+	add eax, 40000h
+	sub eax, 20000h
+	call DumpRegs
+	exit
+main ENDP
+END main
---
+++ chapter3/AddSubPure.asm
@@ -0,0 +1,17 @@
+; this program adds and substracts 32-bit integers.
+
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode: DWORD
+DumpRegs PROTO
+
+.code
+addSub PROC
+	mov eax, 10000h
+	add eax, 40000h
+	sub eax, 20000h
+	call DumpRegs
+	INVOKE ExitProcess, 0
+addSub ENDP
+END addSub
