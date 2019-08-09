aaf992a3b685f4ad2b61ae5e35e1a0d6627344ef
luoheng <1301089462@qq.com>
2019-08-09 16:04:51 +0800

    convert program

---
+++ 正则表达式/2-2.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+print "Enter a temperature (e.g., 32F, 100C): ";
+chomp(my $input = <STDIN>);
+
+if ($input =~ m/^([-+]?[0-9]+)([CF])$/) {
+    my $inputNum = $1;
+    my $type = $2;
+    my $celsius;
+    my $fahrenheit;
+
+    if ($type eq "C") {
+        $celsius = $inputNum;
+        $fahrenheit = ($celsius * 9 / 5) + 32;
+    } else {
+        $fahrenheit = $inputNum;
+        $celsius = ($fahrenheit - 32) * 5 / 9;
+    }
+
+    printf "%.2f C is %.2f F\n", $celsius, $fahrenheit;
+} else {
+    print "Expecting a number followed by \"C\" or \"F\",\n";
+    print "so I don't understand \"$input\".\n";
+}
