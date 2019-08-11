316507a7cbfe62157e53e0b6e25ef93b6919f00b
luoheng <1301089462@qq.com>
2019-08-11 14:55:15 +0800

    add preclare for combine4-7

--- chapter5/combine.h
+++ chapter5/combine.h
@@ -7,3 +7,6 @@ void combine1(vec_ptr v, data_t *dest);
 void combine2(vec_ptr v, data_t *dest);
 void combine3(vec_ptr v, data_t *dest);
 void combine4(vec_ptr v, data_t *dest);
+void combine5(vec_ptr v, data_t *dest);
+void combine6(vec_ptr v, data_t *dest);
+void combine7(vec_ptr v, data_t *dest);
