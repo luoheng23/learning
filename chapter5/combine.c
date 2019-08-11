e19960339c7e087fc4003778e0f17f4c1e24b8e5
luoheng <1301089462@qq.com>
2019-08-11 10:56:15 +0800

    combine3 and combine4 ok

--- chapter5/combine.c
+++ chapter5/combine.c
@@ -25,3 +25,32 @@ void combine2(vec_ptr v, data_t *dest)
         *dest = *dest OP val;
     }
 }
+
+void combine3(vec_ptr v, data_t *dest)
+{
+    long i;
+    long length = vec_length(v);
+    data_t *data = v->data;
+
+    *dest = IDENT;
+    for (i = 0; i < length; i++)
+    {
+        *dest = *dest OP data[i];
+    }
+}
+
+void combine4(vec_ptr v, data_t *dest)
+{
+    long i;
+    long length = vec_length(v);
+    data_t *data = v->data;
+    data_t acc = IDENT;
+
+    *dest = IDENT;
+    for (i = 0; i < length; i++)
+    {
+        acc = acc OP data[i];
+    }
+    *dest = acc;
+}
+
--- chapter5/combine.h
+++ chapter5/combine.h
@@ -5,3 +5,5 @@
 
 void combine1(vec_ptr v, data_t *dest);
 void combine2(vec_ptr v, data_t *dest);
+void combine3(vec_ptr v, data_t *dest);
+void combine4(vec_ptr v, data_t *dest);
