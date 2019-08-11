751551c10e6c60fca75e72011d9b15865afe1e67
luoheng <1301089462@qq.com>
2019-08-11 10:39:20 +0800

    vector test combine1 ok

---
+++ chapter5/combine.c
@@ -0,0 +1,13 @@
+#include "combine.h"
+
+void combine1(vec_ptr v, data_t *dest)
+{
+    long i;
+    *dest = IDENT;
+    for (i = 0; i < vec_length(v); i++)
+    {
+        data_t val;
+        get_vec_element(v, i, &val);
+        *dest = *dest OP val;
+    }
+}
---
+++ chapter5/combine.h
@@ -0,0 +1,6 @@
+#include "vector.h"
+
+#define IDENT 0
+#define OP +
+
+void combine1(vec_ptr v, data_t *dest);
