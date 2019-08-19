e4bfdb6d95ac9dfcfdb8cb1445c12e9c22352b08
luoheng <1301089462@qq.com>
2019-08-19 21:34:30 +0800

    chapter6 ok

--- lang/go/chapter6/6.go
+++ lang/go/chapter6/6.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"math"
 )
 
 // Point is good
 type Point struct{ X, Y float64 }
+
+// ColoredPoint is good
+type ColoredPoint struct {
+	Point
+	Color color.RGBA
+}
+
+func colorPoint() {
+	red := color.RGBA{255, 0, 0, 255}
+	blue := color.RGBA{0, 0, 255, 255}
+	var cp ColoredPoint
+	cp.X = 1
+	var p = ColoredPoint{Point{1, 1}, red}
+	var q = ColoredPoint{Point{5, 4}, blue}
+	fmt.Println(p.Distance(q.Point))
+}
 
 // Distance is good
 func Distance(p, q Point) float64 {
@@ -25,6 +42,12 @@ func geo() {
 	fmt.Println(q.Distance(p))
 }
 
+func testLink() {
+	print(&[3]int{})
+}
+
 func main() {
-	geo()
+	// geo()
+	// testLink()
+	colorPoint()
 }
