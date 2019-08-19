143032aa1efaac5118a1361ebfb3ef0bc43f5cc6
luoheng <1301089462@qq.com>
2019-08-19 22:56:40 +0800

    chapter 7 ok

---
+++ lang/go/chapter7/7.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// ByteCounter is good
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p))
+	return len(p), nil
+}
+
+func fprintf() {
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c)
+	c = 0
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c)
+}
+
+func main() {
+	fprintf()
+}
