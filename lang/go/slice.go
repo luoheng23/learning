ca5862575a74a58669470efa6ad5bceb23cc919e
luoheng <1301089462@qq.com>
2019-08-25 09:12:17 +0800

    go test

--- lang/go/slice.go
+++ lang/go/slice.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var array = [5]int{1, 2, 3, 4, 5}
 	var slice []int
-	var m map[string]int = map[string]int{}
-	slice = append(slice, 2)
-	fmt.Println(slice)
-	m["hello"] = 10
-	fmt.Println(m)
+	slice = array[:2]
+	newSlice := slice[:5]
+	slice = append(slice, 1)
+	fmt.Println(slice, array, newSlice)
 }
