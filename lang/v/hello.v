086affd908d6ce9e204a3b115f723654dfdef3ef
luoheng <1301089462@qq.com>
2019-09-16 22:21:13 +0800

    learning v

--- lang/v/hello.v
+++ lang/v/hello.v
@@ -1,50 +1,11 @@
-fn main() {
-    name := 'Bob'
-    println('Hello, $name!')
-    age := 20
-    large_number := i64(99999)
-    println(name)
-    print(age)
-    print(large_number)
 
-    println('hello world')
-    println(add(77, 53))
-    println(sub(100, 50))
-
-
-    mut nums := [1, 2, 3]
-    nums << 4
-    nums << [5, 6, 7]
-    println(nums.len)
-    println(5 in nums)
-
-    mut m := map[string]int
-    m['one'] = 1
-    m['two'] = 2
-    println(m['one'])
-    println(m['bad_key'])
-    println('bad_key' in m)
-    m.delete('two')
-    m.delete('o')
-
-    println('if:')
-    num := 777
-    s := if num % 2 == 0 { 'even' } else { 'odd' }
-    println(s)
-
-    os := 'windows'
-    print('V is running on ')
-    match os {
-        'darwin' => { println('macOS.') }
-        'linux' => { println('Linux.')}
-        else => { println(os)} 
+fn this(hello ...int) {
+    for j in hello {
+        print(j)
     }
 }
 
-fn add(x int, y int) int {
-    return x + y
-}
-
-fn sub(x, y int) int {
-    return x - y
+fn main() {
+    hello := [1, 2, 3, 4]
+    this(hello...)
 }
