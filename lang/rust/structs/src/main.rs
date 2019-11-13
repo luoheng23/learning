a138fd1de583f7302cab7e45314beced93a65a18
luoheng <1301089462@qq.com>
2019-11-13 15:16:08 +0800

    struct

--- lang/rust/structs/src/main.rs
+++ lang/rust/structs/src/main.rs
@@ -1,4 +1,5 @@
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -9,10 +10,26 @@ impl Rectangle {
         return self.width * self.height;
     }
 }
+
+// struct Color(i32, i32, i32);
+// struct Point(i32, i32, i32);
+
+// struct User {
+//     username: &str,
+//     email: &str,
+//     sign_in_count: u64,
+//     active: bool,
+// }
+
 fn main() {
-    let mut rect1 = Rectangle { width: 30, height: 50};
-    let _rect2 = rect1;
-    rect1 = Rectangle { width: 20, height: 30};
+    let rect1 = Rectangle { width: 30, height: 50};
+    let _rect2 = Rectangle { ..rect1 };
+    // let user1 = User {
+    //     email: "1301089462@qq.com",
+    //     username: "luoheng",
+    //     active: true,
+    //     sign_in_count: 1,
+    // };
     println!("The area of the rectangle is {} square pixels.", rect1.area());
-    println!("{:#?}", rect1);
+    println!("rect1 is {:#?}", rect1);
 }
