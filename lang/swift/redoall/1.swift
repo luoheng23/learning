e4143d54b910ceae6d6d4787b467f9b19e4dfc5f
luoheng <1301089462@qq.com>
2020-01-05 13:29:43 +0800

    add some test

--- lang/swift/redoall/1.swift
+++ lang/swift/redoall/1.swift
@@ -1,3 +1,23 @@
 
 var name = "luo"
 print("\(name)")
+
+let explicit: Float = 70
+print(explicit)
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+print(label, width, widthLabel)
+print(Int("94") ?? 0)
+let max: Int
+max = 3
+print(max)
+
+var (x, y, z) = (1.0, 2, "string")
+print(x, y, z)
+
+typealias audio = UInt16
+
+var s: audio = 16
+print(s + UInt16(16))
