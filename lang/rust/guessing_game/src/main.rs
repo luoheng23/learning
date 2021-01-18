0e792053b7d7db37a081ce765047a85f046a8b27
luoheng <1301089462@qq.com>
2021-01-18 21:17:42 +0800

    practice once more

--- lang/rust/guessing_game/src/main.rs
+++ lang/rust/guessing_game/src/main.rs
@@ -1,7 +1,8 @@
+extern crate rand;
 
 use std::io;
-use rand::Rng;
 use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
@@ -10,19 +11,19 @@ fn main() {
 
     loop {
         println!("Please input your guess.");
-
+    
         let mut guess = String::new();
-
+    
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
-
+    
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        println!("{:?}.", guess.cmp(&secret_number));
-
+    
         println!("You guessed: {}", guess);
+        
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
@@ -31,5 +32,6 @@ fn main() {
                 break;
             }
         }
+
     }
 }
---
+++ lang/rust/hello_cargo/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    for num in (1..4).rev() {
+        println!("Num: {}", num);
+    }
+}
